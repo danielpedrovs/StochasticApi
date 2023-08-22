da7fa737c4f4176f99f27baa70c465c7274e8e25
danielpedrovs <danielpedrovs@gmail.com>
2023-08-23 00:09:33 +0100

    start the project with Angular JS

---
+++ src/services/stockServices.js
@@ -0,0 +1,33 @@
+// StockService.js
+angular.module('myApp.services', [])
+  .service('StockService', function() {
+    this.isOverSold = function(stockH14P, stockL14P, ClosePrice) {
+      function isOverSold(stockH14P, stockL14P, ClosePrice) {
+        const kStock = 100 * ((ClosePrice - stockL14P) / (stockH14P - stockL14P));
+        const kStockMessage = 'the %k is ' + kStock;
+      
+        const sttmentK = kStock < 20
+          ? "That means the stock is oversold and good to buy"
+          : kStock > 80
+          ? "That means the stock is overbought and you can consider selling"
+          : "The stock is neither oversold nor overbought.";
+        
+        const statementMessage = sttmentK;
+      
+        return {
+          kStockMessage,
+          statementMessage
+        };
+      }
+      
+      // Get user input
+      const stockHigh = parseFloat(prompt("Enter the highest price:"));
+      const stockLow = parseFloat(prompt("Enter the lowest price:"));
+      const currentPrice = parseFloat(prompt("Enter the current price:"));
+      
+      // Call the function with user input
+      const result = isOverSold(stockHigh, stockLow, currentPrice);
+      console.log(result.kStockMessage);
+      console.log(result.statementMessage);
+    };
+  });
